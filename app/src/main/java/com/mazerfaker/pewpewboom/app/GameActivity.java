07f7a8a5f35ee3f004caf493a0536dd6ad8b84cf
Max <macsimbodnar@gmail.com>
2017-08-29 16:58:01 +0200

    Creato un layout con tre colonne
    Settato anche i listeners su tali colonne sulle azzioni down e up

--- app/src/main/java/com/mazerfaker/pewpewboom/app/GameActivity.java
+++ app/src/main/java/com/mazerfaker/pewpewboom/app/GameActivity.java
@@ -2,14 +2,80 @@ package com.mazerfaker.pewpewboom.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.mazerfaker.pewpewboom.R;
 
 public class GameActivity extends Activity {
 
+    private static final String TAG = "GameActivity";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        LinearLayout left = (LinearLayout) findViewById(R.id.game_left);
+        LinearLayout center = (LinearLayout) findViewById(R.id.game_center);
+        LinearLayout right = (LinearLayout) findViewById(R.id.game_right);
+
+
+        // TODO left listener
+        left.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    Log.d(TAG, "LEFT DOWN");
+                }
+
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    Log.d(TAG, "LEFT UP");
+                }
+
+                return true;
+            }
+        });
+
+
+        // TODO center listener
+        center.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    Log.d(TAG, "CENTER DOWN");
+                }
+
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    Log.d(TAG, "CENTER UP");
+                }
+
+                return true;
+            }
+        });
+
+
+        // TODO right listener
+        right.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    Log.d(TAG, "RIGHT DOWN");
+                }
+
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    Log.d(TAG, "RIGHT UP");
+                }
+
+                return true;
+            }
+        });
+
     }
 }
